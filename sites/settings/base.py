dc655fd0a9a20cf91acad948eaf7c8436db6f7ab
vicalloy <zbirder@gmail.com>
2012-08-13 20:45:50 +0800

    fix for i18n

--- sites/settings/base.py
+++ sites/settings/base.py
@@ -30,11 +30,13 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+#TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 SITE_ID = 1
 
