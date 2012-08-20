887d8b2dd594adc922d882eee6939c18665e55d3
vicalloy <zbirder@gmail.com>
2012-08-20 10:47:51 +0800

    add unit test

--- sites/dpress/tests.py
+++ sites/dpress/tests.py
@@ -1,16 +1,35 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# -*- coding: UTF-8 -*-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
+class ViewsBaseCase(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    fixtures = ['test_dpress_users.json', 
+            'test_dpress_posts.json']
+
+class ViewsSimpleTest(ViewsBaseCase):
+
+    def test_index(self):
+        resp = self.client.get(reverse('dpress_index'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_archive(self):
+        resp = self.client.get(reverse('dpress_month_archive', args=("2012", "8", )))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_tag(self):
+        resp = self.client.get(reverse('dpress_tag', args=("testtag", )))
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.get(reverse('dpress_tag', args=("notag", )))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_post(self):
+        resp = self.client.get(reverse('dpress_post', args=("2012", "08", "dpress")))
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.get(reverse('dpress_post', args=("2012", "08", "noslug")))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_feed(self):
+        resp = self.client.get(reverse('dpress_feeds'))
+        self.assertEqual(resp.status_code, 200)
+
