b7e691f85a83eea054f9b01f2973b7bba703bbee
vicalloy <zbirder@gmail.com>
2017-03-21 11:21:49 +0800

    format code(pep8)

--- dpress/templatetags/dpress_tags.py
+++ dpress/templatetags/dpress_tags.py
@@ -1,7 +1,4 @@
-import re
-
 from django.template import Library
-from django.utils.safestring import mark_safe
 
 from dpress.helper import tl_markdown
 from dpress.models import Post
@@ -9,28 +6,32 @@ from dpress.models import Category
 
 register = Library()
 
+
 @register.filter
 def md(_md):
     return tl_markdown(_md)
 
+
 @register.inclusion_tag("dpress/tags/dummy.html")
 def last_post(template='dpress/widgets/lastposts.html'):
-    lastposts = Post.objects.filter(status=2).select_related(depth=1).order_by("-publish")
+    lastposts = Post.objects.filter(status=2).select_related().order_by("-publish")
     return {
-            "template": template,
-            'lastposts': lastposts[:5]
+        "template": template,
+        'lastposts': lastposts[:5]
     }
+
 
 @register.inclusion_tag("dpress/tags/dummy.html")
 def month_links(template="dpress/widgets/monthlinks.html"):
     return {
-            "template": template,
-            'dates': Post.objects.filter(status=2).dates('publish', 'month')[:12],
-            }
+        "template": template,
+        'dates': Post.objects.filter(status=2).dates('publish', 'month')[:12],
+    }
+
 
 @register.inclusion_tag("dpress/tags/dummy.html")
 def categorys(template="dpress/widgets/categorys.html"):
     return {
-            "template": template,
-            'categorys': Category.objects.order_by("title"),
-            }
+        "template": template,
+        'categorys': Category.objects.order_by("title"),
+    }
