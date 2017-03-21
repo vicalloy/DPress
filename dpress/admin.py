e67ddc1a61a24cac26ebd2af97e177bbcd2e330d
vicalloy <zbirder@gmail.com>
2017-03-21 10:42:31 +0800

    format code(pep8)

--- dpress/admin.py
+++ dpress/admin.py
@@ -5,18 +5,21 @@ from django.contrib import admin
 from .models import Post, Category
 from .widgets import EpicEditorWidget
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display        = ('title', 'slug', 'author', 'status', 'created_at', 'publish', )
-    search_fields       = ('title', 'body', )
-    #raw_id_fields       = ('author',)
-    #list_filter         = ('category',)
+    list_display = ('title', 'slug', 'author', 'status', 'created_at', 'publish', )
+    search_fields = ('title', 'body', )
+    # raw_id_fields = ('author',)
+    # list_filter = ('category',)
     formfield_overrides = {
         models.TextField: {'widget': EpicEditorWidget},
     }
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    list_display        = ('title', 'slug', )
-    search_fields       = ('title', 'slug', )
+    list_display = ('title', 'slug', )
+    search_fields = ('title', 'slug', )
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
